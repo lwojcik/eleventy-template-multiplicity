413934af3937cecff95ce9d0d990be1350edc548
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-08-14 01:55:17 +0200

    Add MIME type check for avatars

--- _11ty/helpers/cacheAvatar.js
+++ _11ty/helpers/cacheAvatar.js
@@ -2,17 +2,28 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
 const slugify = require("./slugify");
-const sharp = require("sharp"); // Import the sharp library
+const sharp = require("sharp");
 
 const DIST_PATH = "_site";
 const AVATAR_DIR = path.join("images", "avatars");
 const DEFAULT_AVATAR_FILE = "default_avatar.jpg";
 const DEFAULT_AVATAR_PATH = path.join("/", "images", DEFAULT_AVATAR_FILE);
 
+const FILE_EXTENSIONS = {
+  "image/jpeg": "jpg",
+  "image/png": "png",
+  "image/gif": "gif",
+  "image/x-icon": "ico",
+  "image/vnd.microsoft.icon": "ico",
+};
+
+const FILE_EXTENSIONS_NOT_TO_RESIZE = ["svg", "ico"];
+
 module.exports = async ({ url, name }) => {
   try {
-    const avatarExtension = url.split(".").pop();
-    const fileName = `${slugify(name)}.${avatarExtension}`;
+    if (!url.startsWith("https://")) {
+      return DEFAULT_AVATAR_PATH;
+    }
 
     const response = await fetch(url);
 
@@ -22,36 +33,25 @@ module.exports = async ({ url, name }) => {
       );
     }
 
-    const buffer = await response.buffer();
-
     const contentType = response.headers.get("content-type");
-    if (
-      (contentType && contentType.includes("image/svg+xml")) ||
-      (contentType && contentType.includes("image/vnd.microsoft.icon"))
-    ) {
-      const savePath = path.join(DIST_PATH, AVATAR_DIR, fileName);
+    const extension = FILE_EXTENSIONS[contentType];
 
-      const dirPath = path.dirname(savePath);
-      fs.mkdirSync(dirPath, { recursive: true });
-
-      fs.writeFileSync(savePath, buffer);
-
-      return path.join("/", AVATAR_DIR, fileName);
-    }
+    const fileName = `${slugify(name)}.${extension}`;
+    const buffer = await response.buffer();
 
     let resizedBuffer = buffer;
 
-    const image = sharp(buffer);
-    const metadata = await image.metadata();
-    if (metadata.width > 64 || metadata.height > 64) {
-      resizedBuffer = await image.resize(64, 64).toBuffer();
+    if (!FILE_EXTENSIONS_NOT_TO_RESIZE.includes(extension)) {
+      const image = sharp(buffer);
+      const metadata = await image.metadata();
+      if (metadata.width > 64 || metadata.height > 64) {
+        resizedBuffer = await image.resize(64, 64).toBuffer();
+      }
     }
 
     const savePath = path.join(DIST_PATH, AVATAR_DIR, fileName);
-
     const dirPath = path.dirname(savePath);
     fs.mkdirSync(dirPath, { recursive: true });
-
     fs.writeFileSync(savePath, resizedBuffer);
 
     return path.join("/", AVATAR_DIR, fileName);
