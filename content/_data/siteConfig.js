b510f444e51540305aceac0ec2fb53680eae2c86
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-10-04 01:13:40 +0200

    Set enablePWA to false by default (#38)

--- content/_data/siteConfig.js
+++ content/_data/siteConfig.js
@@ -19,6 +19,6 @@ module.exports = {
   },
   maxPostLength: 500, // How many characters per each post excerpt?
   maxItemsPerFeed: 10, // How many items should be fetched from each feed?
-  enablePWA: true, // If true, service worker is registered to make the site behave like a mobile app (PWA)
+  enablePWA: false, // If true, service worker is registered to make the site behave like a mobile app (PWA)
   localCacheDuration: "7d", // For how long should network calls be cached locally? See https://www.11ty.dev/docs/plugins/fetch/#change-the-cache-duration
 };
